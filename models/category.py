53dd6b3b2e9eb56d56c28f4ed6e537f4a86c5943
OsamaSiddique20 <149616071+OsamaSiddique20@users.noreply.github.com>
2023-12-11 17:57:10 +0300

    Update category.py

--- models/category.py
+++ models/category.py
@@ -1,43 +1,33 @@
-
 import sys
 from extensions import db
-
 from http import HTTPStatus
-
 
 class Category(db.Model):
     __tablename__ = 'Category'
-
     cat_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     cat_name = db.Column(db.String(255), nullable=False)
     cat_desc = db.Column(db.Text)
-
-    # items = db.relationship("Item", backref="category")
     items = db.relationship("Item", backref="Category")
-
+    
     @property
     def data(self):
         return {
             'cat_id': self.cat_id,
             'cat_name': self.cat_name,
-            'cat_desc': self.cat_desc,
-        }
-
+            'cat_desc': self.cat_desc}
+        
     def save(self):
         db.session.add(self)
         db.session.commit()
-
+        
     @classmethod
     def get_all(cls):
         r = cls.query.all()
-
         result = []
-
         for i in r:
             result.append(i.data)
-
         return result
-    
+        
     @classmethod
     def get_id_by_id(cls, cat_id):
         print(cat_id)
@@ -46,28 +36,20 @@ class Category(db.Model):
     @classmethod
     def update(cls, cat_id, data):
         category = cls.query.get(cat_id)
-
         if category:
             category.cat_name = data.get('cat_name', category.cat_name)
             category.cat_desc = data.get('cat_desc', category.cat_desc)
-
             db.session.commit()
-
             return category.data
         else:
             return None
 
-
     @classmethod
     def delete(cls, id):
         item  = cls.query.filter(cls.cat_id==id).first()
-
         if item is None:
             return {'message': 'Category not found'}, HTTPStatus.NOT_FOUND
-
         db.session.delete(item)
         db.session.commit()
-        
         updated_items = cls.get_all()
-
         return {'data': updated_items}, HTTPStatus.OK
